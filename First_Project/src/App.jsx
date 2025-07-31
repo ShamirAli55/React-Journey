67b1ae3cd39ef58a5009c99a2754b52ae763e7b3
ShamirAli55 <shamirali9779@gmail.com>
2025-07-31 01:46:19 -0700

    Newly Added

---
+++ First_Project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useState} from 'react';
+const App = () => {
+  return (
+    <>
+        <div className='bg-violet-500 w-full h-screen flex justify-center items-center p-[10vw]'>Hello</div>
+    </>
+  )
+}
+
+export default App
